8113ac2b99eddf2902787b1a488a40f5b2bfa237
simon <simonbw@uw.edu>
2018-02-16 19:37:52 -0800

    Fix bug in DamageFlash

--- src/client/components/game/DamageFlash.js
+++ src/client/components/game/DamageFlash.js
@@ -37,7 +37,7 @@ const DamageFlash = () => (
     select={(state) => ({
       hitPoints: state.getIn([GAME, HIT_POINTS])
     })}
-    render={(hitPoints) => (
+    render={({ hitPoints }) => (
       <UnconnectedDamageFlash hitPoints={hitPoints}/>
     )}
   />
